92c867d459cc04b5e41293fd6c8f06fa1d0857f4
Mete <backendDevEn@protonmail.com>
2026-03-11 23:20:31 +0300

    Variable types explained in python

---
+++ main.py
@@ -0,0 +1,39 @@
+
+# this is my very first python programm
+
+# strings
+email = "fakegmail@gmail.com" 
+name = "Dev"
+
+# integers
+num_of_bombs = 20 
+age = 67 #
+
+print("Hello World!") # first thing in programming
+print(f"Your name is: {name}")
+print(f"Your current age is: {age}") # integer print
+print(email) 
+print(f"Bombs number: {num_of_bombs}")
+
+# floats
+price = 67.99
+discount = 10.99
+gpa = 87.67
+print(f"Student's gpa is {gpa} which is enough for us to accept him")
+print(f"The price: {price} USD and the discount: {discount} USD")
+
+# Booleans
+is_good = True
+
+if is_good: # statement next to "if" has to be true if the goal is to execute something
+    print("Nice!")
+else:
+    print("Sorry to hear that.")
+
+is_cheap = False
+if is_cheap:
+    print("Think of buying it!")
+else:
+    print("That's too expensive for us to buy")
+
+    

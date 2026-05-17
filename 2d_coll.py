6a51baf63b318fa94aa6c4a57a563eba120c609d
Mete <backendDevEn@protonmail.com>
2026-05-17 16:14:30 +0300

    2d collections practiced

---
+++ 2d_coll.py
@@ -0,0 +1,24 @@
+# 2D collections (tuples, lists, sets)
+# 2D Tuple
+num_pad = (
+(1, 2, 3), 
+(4, 5, 6), 
+(7, 8, 9), 
+('*', 0, '#'))
+
+for row in num_pad:
+    for num in row:
+        print(num, end=' ')
+    print()
+
+# 2D List
+hardware = ['motherboard', 'cpu', 'gpu', 'ram', 'ssd', 'cooling fan', 'psu']
+software = ['photoshop', 'premier pro', 'youtube']
+os = 'windows'
+pc = [hardware, software, os]
+
+print(pc)
+
+apps = [{'youtube','youtube'}]
+print(apps)
+
